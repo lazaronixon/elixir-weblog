972d8dd8ae2d30a7587731e14859b344eb46bef7
lazaronixon <lazaronixon@hotmail.com>
2021-02-22 23:29:58 -0300

    controllers like rails

--- lib/weblog_web/controllers/page_controller.ex
+++ lib/weblog_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule WeblogWeb.PageController do
   use WeblogWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render conn, :index
   end
 end
--- lib/weblog_web/controllers/post_controller.ex
+++ lib/weblog_web/controllers/post_controller.ex
@@ -4,59 +4,51 @@ defmodule WeblogWeb.PostController do
   alias Weblog.Posts
   alias Weblog.Posts.Post
 
+  plug :set_post when action in [:show, :edit, :update, :delete]
+
   def index(conn, _params) do
-    posts = Posts.list_posts()
-    render(conn, "index.html", posts: posts)
+    render conn, :index, posts: Posts.list_posts
   end
 
   def new(conn, _params) do
-    changeset = Posts.change_post(%Post{})
-    render(conn, "new.html", changeset: changeset)
+    render conn, :new, changeset: Posts.change_post(%Post{})
   end
 
   def create(conn, %{"post" => post_params}) do
     case Posts.create_post(post_params) do
       {:ok, post} ->
-        conn
-        |> put_flash(:info, "Post created successfully.")
-        |> redirect(to: Routes.post_path(conn, :show, post))
+        conn |> put_flash(:info, "Post created successfully.") |> redirect(to: Routes.post_path(conn, :show, post))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render conn, :new, changeset: changeset
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    post = Posts.get_post!(id)
-    render(conn, "show.html", post: post)
+  def show(conn, _params) do
+    render conn, :show
   end
 
-  def edit(conn, %{"id" => id}) do
-    post = Posts.get_post!(id)
-    changeset = Posts.change_post(post)
-    render(conn, "edit.html", post: post, changeset: changeset)
+  def edit(conn, _params) do
+    render conn, :edit, changeset: Posts.change_post(conn.assigns[:post])
   end
 
-  def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Posts.get_post!(id)
-
-    case Posts.update_post(post, post_params) do
+  def update(conn, %{"post" => post_params}) do
+    case Posts.update_post(conn.assigns[:post], post_params) do
       {:ok, post} ->
-        conn
-        |> put_flash(:info, "Post updated successfully.")
-        |> redirect(to: Routes.post_path(conn, :show, post))
+        conn |> put_flash(:info, "Post updated successfully.") |> redirect(to: Routes.post_path(conn, :show, post))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", post: post, changeset: changeset)
+        render conn, :edit, changeset: changeset
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    post = Posts.get_post!(id)
-    {:ok, _post} = Posts.delete_post(post)
+  def delete(conn, _params) do
+    Posts.delete_post(conn.assigns[:post])
 
-    conn
-    |> put_flash(:info, "Post deleted successfully.")
-    |> redirect(to: Routes.post_path(conn, :index))
+    conn |> put_flash(:info, "Post deleted successfully.") |> redirect(to: Routes.post_path(conn, :index))
+  end
+
+  defp set_post(conn, _params) do
+    assign conn, :post, Posts.get_post!(conn.params["id"])
   end
 end
